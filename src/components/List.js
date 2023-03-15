370c63011d9dc36ed12bb0cb97e20b0aac0fb819
Tamanna Motaleb <tamannamotaleb@Tamannas-MBP.lan>
2023-03-15 10:54:38 -0400

    Updated files for seperation of concerns

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { isOverdue, formattedDate, sortTodos } from "./utilities";
+import { isOverdue, formattedDate, sortTodos } from "../utils/utilities";
 
 const API_KEY = "PMAK-5ef63db179d23c004de50751-10300736bc550d2a891dc4355aab8d7a5c";
 const API_URL = "https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io";
